9b7be621c34fd7dcd49637d42b521781d34628bf
Volkolak <igor.parketmir@gmail.com>
2021-11-23 12:54:03 +0300

    fix sales page

--- resources/views/manufactures.blade.php
+++ resources/views/manufactures.blade.php
@@ -65,29 +65,4 @@
 </section>
       
 @endsection
-
-
-
-{{-- <section class="bg-light-grey products">
-    <div class="wrap">
-        <div class="col-lg-12"><h1>Производители</h1></div>
-        <section class="col-lg-12 row"> 
-        @foreach ($manufactures as $manufacture)
-            <div class="col-lg-4">
-                <div class="card">
-                    <div class="card-header">
-                        <h4><a href="{{ route('manufacture', $manufacture->slug) }}">{{ $manufacture->manufacture }} @if ($manufacture->country != '' || $manufacture->country != NULL)
-                            ({{ $manufacture->country }})
-                       @endif</a></h4>
-                    </div>
-                    <div class="card-body">
-                        <div class="card_description">{{ $manufacture->short_description ?? '' }}</div>
-                    </div>
-                </div>
-            </div>
-        @endforeach
-        </section>
-    </div>
-</section> --}}
-    
     
--- resources/views/sales.blade.php
+++ resources/views/sales.blade.php
@@ -3,34 +3,50 @@
     @parent
     <!-- <script src="{{ asset('js/discount_countdown.js') }}" defer></script> -->
 @endsection
+
 @section('content')
-<section id="firstsection">
-@component('components.breadcrumb')
-    @slot('main') <i class="fas fa-home"></i> @endslot
-    @slot('active') Акции @endslot    
-@endcomponent 
-</section>
-<section class="bg-light-grey products">
-    <div class="wrap">
-        <div class="col-lg-12"><h1>Акции и скидки в "Паркетном мире"</h1></div>
-        <section class="col-lg-12 row"> 
-        @foreach ($sales as $sale)
-            <div class="col-lg-4">
-                <div class="card">
-                    <div class="card-header">
-                        <h4><a href="{{ route('sale', $sale->slug) }}">{{ $sale->discount }} {{ $sale->value }} {{ $sale->rus_type }}</a></h4>
-                    </div>
-                    <div class="card-body">
-                        <div class="card_info card_info_sale_date @if($sale->it_actuality) dark-green @else dark-red  @endif">{{ $sale->start_d_m_y }} - {{ $sale->d_m_y }}@if(!$sale->it_actuality)<br><span class="bold">(акция закончилась!)</span> @endif</div>
-                        <div class="card_description">{{ $sale->short_description ?? '' }}</div>
-                    </div>
-                </div>
+<section class="uk-section uk-section-small">
+    <div class="uk-container">
+        <div class="uk-grid-medium uk-child-width-1-1 uk-grid uk-grid-stack" uk-grid=''>
+            <div class="uk-text-center uk-first-column">
+                <ul class="uk-breadcrumb uk-flex-center uk-margin-remove">
+                    @component('components.breadcrumb')
+                        @slot('main') <i class="fas fa-home"></i> @endslot
+                        @slot('active') Акции @endslot
+                    @endcomponent 
+                </ul>
+                <h1 class="uk-margin-small-top uk-margin-remove-bottom">Акции и скидки в "Паркетном мире"</h1>
             </div>
-        @endforeach
-        </section>
+
+            <div class="uk-grid-small" uk-grid>                
+                <div class="uk-grid-margin uk-first-column uk-width-expand@m">
+                    <div class="uk-grid-medium uk-grid" uk-grid="">
+                            <div class=" uk-grid-small uk-grid-match" uk-grid>
+                                @foreach ($sales as $sale)
+                                    <div class="uk-width-1-3@m uk-padding-small">
+                                        <div class="uk-card uk-card-default uk-card-body ">
+                                            <h3 class="uk-card-title">
+                                                <a href="{{ route('sale', $sale->slug) }}">{{ $sale->discount }} {{ $sale->value }} {{ $sale->rus_type }}</a>
+                                            </h3>
+                                            
+                                            <hr>
+                                            <div class="card_info card_info_sale_date @if($sale->it_actuality) dark-green @else dark-red  @endif">{{ $sale->start_d_m_y }} - {{ $sale->d_m_y }}@if(!$sale->it_actuality)<br><span class="bold">(акция закончилась!)</span> @endif</div>
+                                            <div class="card_description">{{ $sale->short_description ?? '' }}</div>
+                                            <p>{{ $manufacture->short_description ?? '' }}</p>
+                                        </div>
+                                        
+                                    </div>
+                                    
+                                @endforeach
+                            </div>
+                        </div>
+                    </div>
+                </div>                
+            </div>
+
+
+        </div>
     </div>
 </section>
-    
-    
       
 @endsection
