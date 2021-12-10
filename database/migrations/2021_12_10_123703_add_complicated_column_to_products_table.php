6c753c4f1da5bfc6cf91cb87d36c1214eaa1fbd2
Volkolak <igor.parketmir@gmail.com>
2021-12-10 12:45:55 +0300

    add migrations for complicated products

--- database/migrations/2021_12_10_123703_add_complicated_column_to_products_table.php
+++ database/migrations/2021_12_10_123703_add_complicated_column_to_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddParentProductIdColumtToProductsTable extends Migration
+class AddComplicatedColumnToProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AddParentProductIdColumtToProductsTable extends Migration
     public function up()
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->unsignedBigInteger('parentproduct_id')->default(0)->nullable();
+            $table->boolean('complicated')->nullable()->default(false);
         });
     }
 
